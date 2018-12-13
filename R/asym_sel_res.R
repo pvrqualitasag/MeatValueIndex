4db0527692f84ef546834d1a29b6754aa60c28c5
charlotte-ngs <ngs.charlotte@gmail.com>
2018-12-13 11:19:38 +0100

    started function for asymptotic selection response

---
+++ R/asym_sel_res.R
@@ -0,0 +1,27 @@
+###
+###
+###
+###   Purpose: Functions To Compute Asymptotic Selection Response
+###   started: 2018-12-13 (skn and pvr)
+###
+### ################################################################ ###
+
+
+#' @title Compute asymptotic selection response
+#'
+#' @description
+#'
+#' @param
+#'
+compute_asym_selection_response <- function(pvec_economic_values,
+                                            pvec_proportion_selected,
+                                            pvec_generation_interval,
+                                            pvec_number_progeny,
+                                            pvec_proportion_progeny_adult,
+                                            pmat_genetic_varcov,
+                                            pmat_residual_varcov) {
+  result_asym_sel_res <- NULL
+
+
+  return(result_asym_sel_res)
+}
